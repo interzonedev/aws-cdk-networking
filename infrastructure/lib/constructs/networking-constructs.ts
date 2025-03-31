a48b4337301b8ac5910229d7df04966ec0c52477
Mark Markarian <mark.markarian@siriusxm.com>
2025-03-30 23:35:57 -0700

    Created a VPC with public and private subnets.  Created a public and private hosted zone.  Created public and private DNS A and CNAME records. Added a NetworkLoadBalancer.

--- infrastructure/lib/constructs/networking-constructs.ts
+++ infrastructure/lib/constructs/networking-constructs.ts
@@ -1,8 +1,9 @@
 import { Construct } from 'constructs';
-import { APP_NAME } from '../../config/environments';
-import { Stack } from 'aws-cdk-lib';
 import { CommonConstruct, CommonConstructProps } from './common-construct';
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { ARecord, CnameRecord, PrivateHostedZone, PublicHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { getAbbreviatedConstructName, getConstructName } from '../../config/environments';
+import { NetworkLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export interface NetworkingConstructsProps extends CommonConstructProps {
 }
@@ -13,10 +14,74 @@ export class NetworkingConstructs extends CommonConstruct {
 
         const {stackIdFragment} = props;
 
-        new StringParameter(this, 'foo-param', {
-            parameterName: `/app/${APP_NAME}/${stackIdFragment}/foo`,
-            description: `Foo for ${Stack.of(this)}`,
-            stringValue: 'foo'
+        const vpcId = 'vpc';
+        const vpcName = getConstructName(stackIdFragment, vpcId);
+        const vpc = new Vpc(this, vpcId, {
+            vpcName: vpcName,
+            maxAzs: 2,
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: 'public-subnet',
+                    subnetType: SubnetType.PUBLIC
+                },
+                {
+                    cidrMask: 24,
+                    name: 'private-subnet',
+                    subnetType: SubnetType.PRIVATE_WITH_EGRESS // For private with internet access via NAT
+                }
+            ],
+            natGateways: 1 // Required for PRIVATE_WITH_EGRESS subnets
+        });
+
+        const privateVPCSubnets = vpc.selectSubnets({
+            subnetGroupName: 'private-subnet'
+        });
+
+        const publicHostedZone = new PublicHostedZone(this, 'public-hosted-zone', {
+            zoneName: 'public.interzonedev.com'
+        });
+
+        const privateHostedZone = new PrivateHostedZone(this, 'private-hosted-zone', {
+            zoneName: 'private.interzonedev.com',
+            vpc: vpc
+        });
+
+        const loadBalancerId = 'load-balancer';
+        const loadBalancerName = getAbbreviatedConstructName(stackIdFragment, 'lb');
+        const loadBalancer = new NetworkLoadBalancer(this, loadBalancerId, {
+            loadBalancerName: loadBalancerName,
+            vpc: vpc,
+            vpcSubnets: privateVPCSubnets,
+            internetFacing: false
+        });
+
+        // A Record in Public Zone
+        new ARecord(this, 'public-dns-a-record', {
+            zone: publicHostedZone,
+            recordName: 'app',
+            target: RecordTarget.fromIpAddresses('1.2.3.4')
+        });
+
+        // CNAME Record in Public Zone
+        new CnameRecord(this, 'public-dns-cname-record', {
+            zone: publicHostedZone,
+            recordName: 'www',
+            domainName: 'interzonedev.com'
+        });
+
+        // A Record in Private Zone
+        new ARecord(this, 'private-dns-a-record', {
+            zone: privateHostedZone,
+            recordName: 'internal',
+            target: RecordTarget.fromIpAddresses('10.0.0.5')
+        });
+
+        // CNAME Record in Private Zone
+        new CnameRecord(this, 'private-dns-cname-record', {
+            zone: privateHostedZone,
+            recordName: 'service',
+            domainName: 'my-service.private.interzonedev.com'
         });
     }
 }
